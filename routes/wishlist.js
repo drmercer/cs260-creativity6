78e4aae68c994c197f5327a174d94c233f74404b
Dan Mercer <danielmercer1@gmail.com>
2017-12-08 15:42:57 -0700

    Refactored auth-checker middleware into a module.

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(function(req, res, next) {
-	if (!req/*TODO assert authorized*/) {
-		res.sendStatus(403);
-	} else {
-		next();
-	}
-})
+// Authentication checker middleware
+router.use(require('./authChecker'));
 
 router.get('/test', function(req, res, next) {
 	res.send("Hello world!");
