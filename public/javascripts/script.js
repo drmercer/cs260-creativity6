4c3a85eb3673b1299e4e035c905a86be4b4ae0ba
Dan Mercer <danielmercer1@gmail.com>
2017-12-08 17:01:50 -0700

    Simplified firebase code using $firebaseAuth

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,8 +2,10 @@ angular
 .module('myApp', ["firebase"])
 .controller('myCtrl', myCtrl);
 
-function myCtrl($scope, $firebaseArray) {
+function myCtrl($scope, $firebaseAuth) {
 	$scope.test = "Hello World!";
+
+	$scope.authObj = $firebaseAuth();
 	initFirebase($scope);
 }
 
@@ -12,49 +14,27 @@ function myCtrl($scope, $firebaseArray) {
 
 function initFirebase($scope) {
 
-	$scope.signInStatus = firebase.auth().currentUser ? "Signed in!" : "Not signed in";
+	$scope.signInStatus = "";
 	$scope.signingIn = false;
+	$scope.user = null;
+
 	$scope.toggleSignIn = function toggleSignIn() {
 		$scope.signingIn = true;
-		if (!firebase.auth().currentUser) {
+		if (!$scope.authObj.$getAuth()) {
 			var provider = new firebase.auth.GithubAuthProvider();
 			console.log("Signing in");
-			firebase.auth().signInWithRedirect(provider);
+			$scope.authObj.$signInWithRedirect(provider);
 		} else {
 			console.log("Signing out");
-			firebase.auth().signOut();
+			$scope.authObj.$signOut();
 		}
 	};
 
-	$scope.user = null;
-	firebase.auth().getRedirectResult().then(function(result) {
-		// The signed-in user info.
-		$scope.signingIn = false;
-		$scope.user = result.user;
-		$scope.signInStatus = "Signed in!";
-		$scope.$apply();
-	}).catch(function(error) {
-		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		// The email of the user's account used.
-		var email = error.email;
-		// The firebase.auth.AuthCredential type that was used.
-		var credential = error.credential;
-		if (errorCode === 'auth/account-exists-with-different-credential') {
-			alert('You have already signed up with a different auth provider for that email.');
-			// If you are using multiple auth providers on your app you should handle linking
-			// the user's accounts here.
-		} else {
-			console.error(error);
-		}
-	});
 	// Listening for auth state changes.
-	firebase.auth().onAuthStateChanged(function(user) {
+	$scope.authObj.$onAuthStateChanged(function(user) {
 		console.log("auth state changed:", user);
 		$scope.signInStatus = user ? "Signed in!" : "Not signed in";
 		$scope.user = user || null;
 		$scope.signingIn = false;
-		$scope.$apply() 
 	});
 }
